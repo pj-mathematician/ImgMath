f22e1e51d316d24c951e1c232e7120ca36cafe4d
Pj-mathematician <ojupuju@gmail.com>
2020-08-29 23:05:16 +0530

    updated

--- app/src/main/java/com/PjMathematician/ImgMath/MainActivity.java
+++ app/src/main/java/com/PjMathematician/ImgMath/MainActivity.java
@@ -2,13 +2,33 @@ package com.PjMathematician.ImgMath;
 
 import android.app.*;
 import android.os.*;
+import android.view.*;
+import android.view.View.*;
+import android.widget.*;
 
+import io.github.kexanie.library.MathView;
 public class MainActivity extends Activity 
-{
+{ 
+    MathView formula_one;
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+		final TextView textView = (TextView) findViewById(R.id.output_text);
+        Button button = (Button) findViewById(R.id.convert_button);
+        final EditText txt = (EditText)findViewById(R.id.main_input);
+        button.setOnClickListener(new OnClickListener() {
+
+				@Override
+				public void onClick(View p1)
+				{
+					String tex=txt.getText().toString();
+					textView.setText(tex);
+					formula_one = (MathView) findViewById(R.id.formula_one);
+					formula_one.setText(tex);
+				}
+			});
     }
 }
+
